ca609507ad4d767035ad734de246160f20bca25d
Aryan-yadav7 <yadavaryan1931@gmail.com>
2025-03-16 18:18:25 +0530

    Add files via upload

---
+++ ques3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str = "hello world"; 
+    int n = str.length();
+
+    char charArr[n]; // Array to store unique characters
+    int countArr[n] = {0}; // Array to store count of each character
+    int uniqueCounter = 0; // Unique character count
+
+
+    for (int i = 0; i < n; i++) {
+        char ch = str[i];
+        bool found = false;
+
+        for (int j = 0; j < uniqueCounter; j++) {
+            if (charArr[j] == ch) {
+                countArr[j]++; 
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            charArr[uniqueCounter] = ch;
+            countArr[uniqueCounter] = 1;
+            uniqueCounter++;
+        }
+    }
+
+    cout << "Character  Count\n";
+    for (int i = 0; i < uniqueCounter; i++) {
+        cout << "   " << charArr[i] << "         " << countArr[i] << endl;
+    }
+
+    return 0;
+}
